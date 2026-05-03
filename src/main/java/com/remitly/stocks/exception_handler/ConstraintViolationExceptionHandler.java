f597f90a4b77f887abd56de057e5cd9de950ddf6
Feq39 <konrad.ciborowski149@gmail.com>
2026-05-04 00:12:07 +0200

    some cleanup + additional validation

---
+++ src/main/java/com/remitly/stocks/exception_handler/ConstraintViolationExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.remitly.stocks.exception_handler;
+
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ConstraintViolationExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {ConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConstraintViolation(ConstraintViolationException e, WebRequest request) {
+        return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {Exception.class})
+    protected ResponseEntity<Object> handleUncaughtExceptions(Exception e,WebRequest request) {
+        return handleExceptionInternal(e, "uncaught exception", new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR, request);
+    }
+}
